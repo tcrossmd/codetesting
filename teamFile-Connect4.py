b6c7bcd5e3e40f20dfc9aa49204251136eeb8619
tcrossmd <32273322+tcrossmd@users.noreply.github.com>
2021-04-19 01:41:38 -0400

    Update teamFile-Connect4.py
    4/19/21 - Tara - the board has been created. I sent over the email to the layout. I am working on adding players now. red and black chips. I will be using he tic tac toe board for a start.

--- teamFile-Connect4.py
+++ teamFile-Connect4.py
@@ -1,45 +1,40 @@
 # Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
 # Type "help", "copyright", "credits" or "license()" for more information.
-#Tara - I am building the board. I should be finish with the board on by Sunday.
 
 import turtle
 
+
+turtle.title("The Team Connect 4 Game")
+
 pen_color = str()
-fill_color = str()
 pen_size = int()
 
 #the board for connect four 
 #board should be 7 x 6
+turtle.setx(-350) 
+turtle.right(180)
+turtle.pencolor('blue')
+turtle.pensize(2)
+#line 1
+turtle.right(90)
+turtle.forward(350)
+#line across
+turtle.right(90)
+turtle.forward(100)
+#line down
+turtle.right(90)
+turtle.forward(350)
+x=1
+while x < 7:
+    turtle.back(350)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(350)
+    x = x + 1
+    
+turtle.right(90)
+turtle.forward(702)
+turtle.end_fill()
+
 #squares
-for i in range(2):
-    #turtle.goto(-210,150) #this is the first square
-    turtle.penup()
-    turtle.right(90)
-    turtle.forward(2)
-    turtle.pendown()
-    
-    turtle.pencolor('blue')
-    turtle.pensize(5)
-    #turtle.bgcolor('blue')
-    turtle.fillcolor('blue')
-    turtle.begin_fill()
-    
-    turtle.right(90)
-    turtle.forward(50)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(50)
-    
-    turtle.end_fill()
-    #circle for the board
-    turtle.fillcolor('white')
-    turtle.begin_fill()
-    turtle.circle(50)
-    turtle.end_fill()
-    
-        
